980af2c3c9e8aaabad07a60ff33a091fc1701847
yazid_ha <63234062+yaz-gear@users.noreply.github.com>
2026-01-20 12:20:52 +0000

    Create mqtt_client.cpp

---
+++ src/esp32/mqtt_client.cpp
@@ -0,0 +1,28 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+const char* mqtt_server = "RASPBERRY_PI_IP";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    if (client.connect("ESP32Client")) {
+      Serial.println("Connected to MQTT broker");
+    } else {
+      delay(5000);
+    }
+  }
+}
